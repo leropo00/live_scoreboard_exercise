4100e722d3f818298982a86b4e5938c88e7a43c4
leropo00 <leropo00@gmail.com>
2025-01-15 19:01:34 +0100

    green: added equals and hashcode implementation for the object

--- src/main/java/si/sportradar/scoreboard/Team.java
+++ src/main/java/si/sportradar/scoreboard/Team.java
@@ -1,18 +1,39 @@
 package si.sportradar.scoreboard;
+
+import java.util.Objects;
+
 /**  
  * Team class encapsulates the team info.
  * While based on current scoreboard requirements a String would be enough,
  * class was use instead, in case of future requirements changes
  */
 public class Team {
-
+	//  team name is considered to be unique identifier
 	private String name;
-	
+
 	public Team(String name) {
-		
+		this.name = name;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+	    if (o == this) {
+	        return true;
+	    }
+	    if (!(o instanceof Team)) {
+	        return false;
+	    }
+	    Team other = (Team)o;
+	    return Objects.equals(this.name, other.getName());
+	}
+
+	@Override
+	public int hashCode() {
+	    return Objects.hash(name);
+	}
+
 }
