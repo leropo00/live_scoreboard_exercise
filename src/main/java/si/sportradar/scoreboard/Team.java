54f23158b0156b81356593562c3dce1d7deb60e3
leropo00 <leropo00@gmail.com>
2025-01-15 17:07:16 +0100

    refactor: added comments to explain the logic behind structure decisions and changed field visibility

--- src/main/java/si/sportradar/scoreboard/Team.java
+++ src/main/java/si/sportradar/scoreboard/Team.java
@@ -1,11 +1,14 @@
 package si.sportradar.scoreboard;
-
+/**  
+ * Team class encapsulates the team info.
+ * While based on current scoreboard requirements a String would be enough,
+ * class was use instead, in case of future requirements changes
+ */
 public class Team {
 
-	public String name;
+	private String name;
 	
 	public Team(String name) {
-		
 		
 	}
 }
