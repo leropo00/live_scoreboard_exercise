ca62ee125ab4cc544d3dfdd90a25c2b2be52f2e0
leropo00 <leropo00@gmail.com>
2025-01-15 16:30:12 +0100

    green: initialized empty scoreboard at the start

--- src/main/java/si/sportradar/scoreboard/WorldCupScoreboard.java
+++ src/main/java/si/sportradar/scoreboard/WorldCupScoreboard.java
@@ -1,5 +1,20 @@
 package si.sportradar.scoreboard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WorldCupScoreboard {
 
+	private List<Match> scoreboard;
+	
+	public WorldCupScoreboard() {
+		
+		this.scoreboard = new ArrayList();
+	}
+
+	public List<Match> getScoreboard()  {
+		
+		return this.scoreboard;
+	}
+	
 }
