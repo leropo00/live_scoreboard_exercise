dc1403cdee1c640b4c1d2eb18a3a053387d99ff3
leropo00 <leropo00@gmail.com>
2025-01-15 19:33:38 +0100

    red: prepared tests for match object initialization

--- src/main/java/si/sportradar/scoreboard/Match.java
+++ src/main/java/si/sportradar/scoreboard/Match.java
@@ -1,9 +1,27 @@
 package si.sportradar.scoreboard;
 
 public class Match {
+	public Match(Team homeTeam, Team awayTeam) {
+		
+	}
+	
+	public Team getHomeTeam() {
+		return null;
+	}
+	
+	public Team getAwayTeam() {
+		return null;
+	}
+	
+	public Integer getHomeScore() {
+		return null;
+	}
 
-	public Match(Team homeTeam, Team awayTeam)  {
-		
-		
+	public Integer getAwayScore() {
+		return null;
+	}
+
+	public Integer getTotalScore() {
+		return null;
 	}
 }
---
+++ src/test/java/si/sportradar/scoreboard/MatchTest.java
@@ -0,0 +1,25 @@
+package si.sportradar.scoreboard;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MatchTest {
+
+	@Test
+	@DisplayName("Tests initialization of match object")
+	void testInitialization() {
+		Team homeTeam = new Team("Mexico");
+		Team awayTeam = new Team("Canada");
+		
+		Match match = new Match(homeTeam, awayTeam);
+		
+		assertSame(homeTeam, match.getHomeTeam());
+		assertSame(awayTeam, match.getAwayTeam());
+		assertEquals(0, match.getHomeScore());
+		assertEquals(0, match.getAwayScore());
+		assertEquals(0, match.getTotalScore());
+	}
+}
