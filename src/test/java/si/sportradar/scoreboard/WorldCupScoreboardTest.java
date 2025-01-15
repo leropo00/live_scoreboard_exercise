e732210340dc1150ecb6d8bf5869c2138626dc6e
leropo00 <leropo00@gmail.com>
2025-01-15 16:22:01 +0100

    red: added inital test for scoreboard initalization

--- src/test/java/si/sportradar/scoreboard/WorldCupScoreboardTest.java
+++ src/test/java/si/sportradar/scoreboard/WorldCupScoreboardTest.java
@@ -1,5 +1,23 @@
 package si.sportradar.scoreboard;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class WorldCupScoreboardTest {
 
+	private WorldCupScoreboard scoreboard;
+	
+	@BeforeEach
+	void init() {
+		scoreboard = new WorldCupScoreboard();
+	}
+	
+	@Test 
+	void noMatchesAtStart()  {
+		
+		assertTrue(scoreboard.getScoreboard().isEmpty());
+	}
+	
 }
