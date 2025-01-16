f5fd118d282106c12376c3e7d5571acad87e7141
leropo00 <leropo00@gmail.com>
2025-01-16 15:23:15 +0100

    green: implemented logic for checking of duplicate team in scoreboard

--- src/main/java/com/sportradar/scoreboard/WorldCupScoreboard.java
+++ src/main/java/com/sportradar/scoreboard/WorldCupScoreboard.java
@@ -3,6 +3,8 @@ package com.sportradar.scoreboard;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sportradar.scoreboard.errors.ScoreboardException;
+
 public class WorldCupScoreboard {
 	private List<Match> scoreboard;
 	
@@ -10,7 +12,13 @@ public class WorldCupScoreboard {
 		this.scoreboard = new ArrayList<>();
 	}
 	
-	public void startNewMatch(Team homeTeam, Team awayTeam)  {
+	public void startNewMatch(Team homeTeam, Team awayTeam) throws ScoreboardException {
+		for (Match match : this.scoreboard) {
+			if (match.getHomeTeam().equals(homeTeam) || match.getHomeTeam().equals(awayTeam) || 
+				match.getAwayTeam().equals(homeTeam) || match.getAwayTeam().equals(awayTeam)) {
+				throw new ScoreboardException(ScoreboardException.ERROR_DUPLICATE_TEAM);
+			}
+		}
 		this.scoreboard.add(new Match(homeTeam, awayTeam));
 	}
 
